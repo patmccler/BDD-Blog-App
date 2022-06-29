efbefa474aed0a042ea5465167dc3387742e1ba9
Pat McClernan <patrick.mcclernan@powerhrg.com>
2022-06-29 09:22:13 -0400

    Hide links from nonowner users

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -3,12 +3,11 @@ require "rails_helper"
 RSpec.feature "Listing Articles", :aggregate_failures do
   before do
     @john = User.create!(email: "john@exmaple.com", password: "password")
-    login_as(@john)
     @article1 = Article.create(title: "First Article", body: "Lorem ipsum dolor sit", user: @john)
     @article2 = Article.create(title: "Second Article", body: "Body of second article", user: @john)
   end
 
-  scenario "a user lists all articles" do
+  scenario "with articles created and user not signed in" do
     visit "/"
 
     expect(page).to have_content(@article1.title)
@@ -18,6 +17,21 @@ RSpec.feature "Listing Articles", :aggregate_failures do
 
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
+    expect(page).not_to have_link("New Article")
+  end
+
+  scenario "with articles created and user signed in" do
+    login_as(@john)
+    visit "/"
+
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article1.body)
+
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).to have_link("New Article")
   end
 
   scenario "a user has no articles" do
--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -3,17 +3,48 @@ require "rails_helper"
 RSpec.feature "Creating Articles" do
   before do
     @john = User.create!(email: "john@exmaple.com", password: "password")
-    login_as(@john)
+    @bob = User.create!(email: "bob@exmaple.com", password: "pword123")
+
     @article = Article.create(title: "First Article", body: "Once upon a time...", user: @john)
   end
 
-  scenario "A user views an article" do
+  scenario "to non-signed in user, hide edit and delete buttons" do
     visit "/"
 
     click_link @article.title
 
     expect(page).to have_content(@article.title)
     expect(page).to have_content(@article.body)
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+
+    expect(current_path).to eq("/articles/#{@article.id}")
+  end
+
+  scenario "non-owner user, hide edit and delete buttons" do
+    login_as(@bob)
+    visit "/"
+
+    click_link @article.title
+
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+
+    expect(current_path).to eq("/articles/#{@article.id}")
+  end
+
+  scenario "owner user, hide edit and delete buttons" do
+    login_as(@john)
+    visit "/"
+
+    click_link @article.title
+
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(page).to have_link("Edit Article")
+    expect(page).to have_link("Delete Article")
 
     expect(current_path).to eq("/articles/#{@article.id}")
   end
